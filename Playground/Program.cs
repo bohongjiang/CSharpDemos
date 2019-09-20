44f59bac47860b576a1c901d6bb08907b0b88dab
bohong.development <bohong.development@gmail.com>
2019-09-20 15:34:17 +0800

    added test code to Main function.

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,7 +6,13 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var demo = new Demo1();
+
+            demo.Func1();
+            demo.Func1("hello world;");
+            demo.Func1(1111);
+
+            Console.ReadKey();
         }
     }
 }
